9e2f14da8e5cec5797b780e9a8a083f6f7f7cb0e
ronniejain <jainronak472@gmail.com>
2025-10-10 15:01:26 +0530

    first file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/info")
+def lwinfo():
+    return "i am LW from INdia"
+   
+@app.route("/phone") 
+def lwphone():
+    return "8947002033"
+    
+  
+app.run(host="0.0.0.0")
